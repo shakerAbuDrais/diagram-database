cc2f9b6fd64690d8ef3d8e611d4c8ae33db2de4b
shakerAbuDrais <thecoffeiner@gmail.com>
2022-12-14 17:57:30 +0300

    Create tables and perform queiries

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,52 @@
+CREATE TABLE patients (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR,
+  name VARCHAR,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE treatment_history (
+  medical_history_id INT REFERENCES medical_histories(id),
+  treatment_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id),
+  PRIMARY KEY(id)
+);
+
+CREATE INDEX patient_histories ON medical_histories(patient_id);
+CREATE INDEX ON treatment_history (history_id);
+CREATE INDEX ON treatment_history (treatment_id);
+CREATE INDEX invoice_history ON invoices(medical_history_id);
+CREATE INDEX invoice_treatment_index ON invoice_items(invoice_id, treatment_id);
+
