2a44e77b043173d4e2199fded94d367636a5f499
Alhajideen <salifunurudeen834@gmail.com>
2022-12-14 10:56:28 -0600

    create invoice_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -44,8 +44,3 @@ CREATE TABLE invoice_items (
   PRIMARY KEY(id)
 );
 
-CREATE INDEX patient_histories ON medical_histories(patient_id);
-CREATE INDEX ON treatment_history (history_id);
-CREATE INDEX ON treatment_history (treatment_id);
-CREATE INDEX invoice_history ON invoices(medical_history_id);
-CREATE INDEX invoice_treatment_index ON invoice_items(invoice_id, treatment_id);
